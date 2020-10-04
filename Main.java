e81a228406296641c63d6b0ea515e09d5885e444
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:50:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Keep running until a negative number is entered
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    int userNum;
+    do{
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+      userNum = input.nextInt();
+    }while(userNum > 0);
+
+    // using a loop to keep asking the question
+    while(userNum > 0){
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+    }
+    System.out.println("All Done");
     
   }
 }
